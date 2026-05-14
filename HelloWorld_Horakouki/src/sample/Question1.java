9ac9664784b49c54e794ed5b025779561b0c83a4
uqkq7 <uqkq7@Horakouki0216>
2026-05-14 20:59:16 +0900

    java Q1

---
+++ HelloWorld_Horakouki/src/sample/Question1.java
@@ -0,0 +1,42 @@
+package sample;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+} byte byteNum=0;
+
+ short shortNum=0;
+
+ int intNum=0;
+ 
+ long longNum=0L;
+ 
+ float  floatNum=0f;
+ 
+ double doubleNum=0.0;
+ 
+ char letter='A';
+ 
+ String letters="Java";
+ 
+ boolean isBoolean=true;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
