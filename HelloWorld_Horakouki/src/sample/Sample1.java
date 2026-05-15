a8d5846ee54efd46d7fc29b6b34fa04f8bff3102
uqkq7 <uqkq7@Horakouki0216>
2026-05-15 22:08:39 +0900

    java Q1

--- HelloWorld_Horakouki/src/sample/Sample1.java
+++ HelloWorld_Horakouki/src/sample/Sample1.java
@@ -1,13 +1,10 @@
 package sample;
 
-public class Sample {
+public class Sample1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-	    System.out.println("Hello, World");
 
-	    bcjisaaaaaaaaaaaaa
-	    
 	}
 
 }
